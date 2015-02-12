25fe8c7b075bcd04771bf9988c49143329ef07aa
Chris Sarbora <sarbs@cyngn.com>
2015-02-12 00:49:03 -0800

    Point client socket.io impl at :8081

--- web/client.js
+++ web/client.js
@@ -1,5 +1,5 @@
 var io = require('socket.io-client');
-var socket = io.connect('http://128.95.80.175:8080');
+var socket = io.connect('http://128.95.80.175:8081');
 socket.on('logcat', function (data) {
 	console.log(data);
 });
