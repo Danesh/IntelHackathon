e955d3ae2a12f803c7d7d9080b6d23e1db556291
Chris Sarbora <sarbs@cyngn.com>
2015-02-11 20:34:07 -0800

    Update API

--- hydra/monkey/monkeybot.js
+++ hydra/monkey/monkeybot.js
@@ -5,19 +5,32 @@ var ffi = require("ffi")
 
 // This is the API for the MonkeyBot control library
 var libmonkey = ffi.Library(process.cwd() + '/libmonkeybot.so', {
+    "monkey_start": [ 'bool', [] ],
+    "monkey_stop": [ 'bool', [] ],
     "monkey_down": [ 'bool', [] ],
     "monkey_up": [ 'bool', [] ],
-    "monkey_rotate_right": [ 'bool', [ 'int' ] ],
-    "monkey_rotate_left": [ 'bool', [ 'int' ] ],
-    "monkey_move_to": [ 'bool', [ 'int', 'int' ] ]
-    "monkey_power_hold_at": [ 'bool', [ 'int' ] ]
-    "monkey_get_stats": [ 'string', [] ]
+    "monkey_tap": [ 'bool', [] ]
+    "monkey_rotate": [ 'bool', [ 'int' ] ],
+    "monkey_move_to": [ 'bool', [ 'int', 'int' ] ],
+    // "monkey_power_hold_at": [ 'bool', [ 'int' ] ],
+    "monkey_get_stats": [ 'string', [] ],
+    "monkey_fling_shit": [ 'bool', [] ]
 });
 
 exports.monkeybot = function() {
     //
     // API calls: Note these are all blocking functions
     //
+
+    var monkey_start = function() {
+        console.log("Invoking " + arguments.callee.name)
+        return libmonkey.monkey_start()
+    }
+
+    var monkey_stop = function() {
+        console.log("Invoking " + arguments.callee.name)
+        return libmonkey.monkey_stop()
+    }
 
     var monkey_down = function() {
         console.log("Invoking " + arguments.callee.name)
@@ -29,14 +42,14 @@ exports.monkeybot = function() {
         return libmonkey.monkey_up()
     }
 
-    var monkey_rotate_right = function(angle) {
-        console.log("Invoking " + arguments.callee.name + " with " + JSON.stringify(arguments))
-        return libmonkey.monkey_rotate_right(angle)
+    var monkey_tap = function() {
+        console.log("Invoking " + arguments.callee.name)
+        return libmonkey.monkey_tap()
     }
 
-    var monkey_rotate_left = function(angle) {
+    var monkey_rotate = function(angle) {
         console.log("Invoking " + arguments.callee.name + " with " + JSON.stringify(arguments))
-        return libmonkey.monkey_rotate_left(angle)
+        return libmonkey.monkey_rotate(angle)
     }
 
     var monkey_move_to = function(x, y) {
@@ -44,24 +57,32 @@ exports.monkeybot = function() {
         return libmonkey.monkey_move_to(x, y)
     }
 
-    var monkey_power_hold_at = function(level) {
-        console.log("Invoking " + arguments.callee.name + " with " + JSON.stringify(arguments))
-        return libmonkey.monkey_power_hold_at(level)
-    }
+    // var monkey_power_hold_at = function(level) {
+    //     console.log("Invoking " + arguments.callee.name + " with " + JSON.stringify(arguments))
+    //     return libmonkey.monkey_power_hold_at(level)
+    // }
 
     var monkey_get_stats = function() {
         console.log("Invoking " + arguments.callee.name + " with " + JSON.stringify(arguments))
         return libmonkey.monkey_get_stats()
     }
 
+    var monkey_fling_shit = function() {
+        console.log("Invoking " + arguments.callee.name)
+        return libmonkey.monkey_fling_shit()
+    }
+
     return {
+        monkey_start: monkey_start,
+        monkey_stop: monkey_stop,
         monkey_down: monkey_down,
         monkey_up: monkey_up,
-        monkey_rotate_right: monkey_rotate_right,
-        monkey_rotate_left: monkey_rotate_left,
+        monkey_tap: monkey_tap,
+        monkey_rotate: monkey_rotate,
         monkey_move_to: monkey_move_to,
-        monkey_power_hold_at: monkey_power_hold_at,
-        monkey_get_stats: monkey_get_stats
+        // monkey_power_hold_at: monkey_power_hold_at,
+        monkey_get_stats: monkey_get_stats,
+        monkey_fling_shit: monkey_fling_shit
     }
 }()
 
